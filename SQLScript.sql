c170496511b0e3cb1cfe5dc53f60c4149776b33c
christian-hall <christian.hall395@gmail.com>
2021-02-20 18:24:05 -0500

    added login information

--- SQLScript.sql
+++ SQLScript.sql
@@ -3,7 +3,11 @@ CREATE database PASSWORDMANAGER;
 USE PASSWORDMANAGER;
 
 CREATE table PASSWORDS (
-	id			int 		not null auto_increment,
+	id			int 		primary key  auto_increment,
     password	varchar(20) not null,
     timestamp	datetime	not null
 );
+
+DROP USER IF EXISTS rpg_user@localhost;
+CREATE USER rpg_user@localhost IDENTIFIED BY 'sesame';
+GRANT SELECT, INSERT ON PASSWORDMANAGER.* TO rpg_user@localhost;
