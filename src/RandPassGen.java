65760e9cba3fb00ac81cd4101eadc4d44675325d
christian-hall <christian.hall395@gmail.com>
2021-02-20 09:59:47 -0500

    added code to get password character options dynamically

--- src/RandPassGen.java
+++ src/RandPassGen.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RandPassGen {
 
@@ -23,6 +26,7 @@ public class RandPassGen {
 					String lowercase = Console.getString("Will there be lowercase letters in this password? (y/n): ", "y", "n");
 					String uppercase = Console.getString("Will there be uppercase letters in this password? (y/n): ", "y", "n");
 					String special = Console.getString("Will there be special characters in this password? (y/n): ", "y", "n");
+					String password = getPassword(characters, numbers, lowercase, uppercase, special);
 				} else if (menu.equalsIgnoreCase("l")) {
 					// retrieve passwords from SQL
 				} else if (menu.equalsIgnoreCase("a")) {
@@ -39,6 +43,39 @@ public class RandPassGen {
 		}
 		System.out.println("Goodbye");
 
+	}
+	
+	private static String getPassword(int characters, String numbers, String lowercase, String uppercase, String special) {
+		String password = "";
+		List<String> passoptions = new ArrayList<>();
+		if (numbers.equalsIgnoreCase("y")) {
+			List<String> passnumbers = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+			for (int i = 0; i < passnumbers.size(); i++) {
+				passoptions.add(passnumbers.get(i));
+			}
+		}
+		if (lowercase.equalsIgnoreCase("y")) {
+			List<String> passlowercs = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+			for (int i =0; i < passlowercs.size(); i++) {
+				passoptions.add(passlowercs.get(i));
+			}
+		}
+		if (uppercase.equalsIgnoreCase("y")) {
+			List<String> passuppercs = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+			for (int i = 0; i < passuppercs.size(); i++) {
+				passoptions.add(passuppercs.get(i));
+			}
+		}
+		if (special.equalsIgnoreCase("y")) {
+			List<String> passspecial = Arrays.asList("@", "%", "+", "\\", "/", "'", "!", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~", "-", "_", ".");
+			for (int i = 0; i < passspecial.size(); i++) {
+				passoptions.add(passspecial.get(i));
+			}
+		}
+		for (int i = 0; i < characters; i++) {
+			// append random characters to password
+		}
+		return password;
 	}
 
 }
